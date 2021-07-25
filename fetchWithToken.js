1e9c9f97696c05f24b88dcaaa1fdb5ec17fcf57f
Taisuke Fukuno <fukuno@jig.jp>
2021-07-25 21:21:46 +0900

    update

---
+++ fetchWithToken.js
@@ -0,0 +1,14 @@
+const fetchWithToken = async (accessToken, url, json) => {
+  const headers = {
+    "Accept": "application/json",
+    "Authorization": "token " + accessToken,
+  };
+  if (!json) {
+    return await (await fetch(url, { headers })).json();
+  }
+  headers["Content-Type"] = "application/json";
+  const body = JSON.stringify(json);
+  const method = "PUT";
+  return await (await fetch(url, { method, headers, body })).json();
+};
+export { fetchWithToken };
